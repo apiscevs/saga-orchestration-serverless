19f31105d31f3440e2c0adb85a233481200c381a
apiscevs <aleksejs.piscevs@gmail.com>
2022-03-27 20:31:04 +0200

    Revert "trigger"
    This reverts commit 7e278d80ca500b08800ae3a7451b62959fce69ce.

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "sagalogic-resource-group" {
   name     = "${var.prefix}-resources"
   location = var.location
- 
+
   tags = {
     environment = var.environment
   }
