f11dcd77a338309ec89328cbf1c69e36778f1dbd
apiscevs <aleksejs.piscevs@gmail.com>
2022-03-27 20:09:30 +0200

    access key

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     # modify the values for the storage account
-    storage_account_name = "sagaorchestratorstorage"
+    storage_account_name = "sagaorcsa"
     container_name       = "testingterraform"
-    key                  = "terraform.tfstate"
+    key                  = "SVskX82RVLKKBdqyWsP4swUcp6n1q4PD02c/xHF3kpLNNvU5144veiNzB66weLsHmdo7x1Q2MvooD+++w+j3ig=="
   }
 }
